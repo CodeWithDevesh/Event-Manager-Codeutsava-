fd13ae3c4c0987950318fea1e11ac8a01b4f4695
vsmay-01 <mayankverma5002@gmail.com>
2023-11-02 11:50:49 +0530

    adminsite created

---
+++ Static/adminsite_script.js
@@ -0,0 +1,12 @@
+// let eventList = document.getElementById("event-list");
+
+
+// for (i = 0; i < 10; i++) {
+//     const li = document.createElement('li');
+    
+//     li.setAttribute('class', 'event');
+
+//     li.innerHTML = `<h3>Event ${i}</h3> <p>Date: 2023-11-10</p> <p>Description: This is the description of Event 1.</p>`;
+//     eventList.appendChild(li);
+// }
+
